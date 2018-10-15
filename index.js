e2671c218277a55e00fb39e9682d2971d0d16144
SukachevAlex <sukachevalexandr@gmail.com>
2018-10-16 02:19:13 +0300

    По роуту /status выдается время, прошедшее с запуска сервера
    Добавил два метода, один из которых позвращает разницу между временем запуска сервера и переходом п$
    Второй метод переводит миллисекунды в формат hh:mm::ss

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const startingDate = new Date();
+
+app.all('/status', (req, res) => {
+    let diff = getDiffBetweenDates(startingDate, new Date());
+    res.send(msToReadableTime(diff));
+})
 
 app.all('*', (req, res) => {
     res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(port, () => console.log(`Working on port ${port}`));
+
+
+function getDiffBetweenDates(startDate, curDate) {
+    return curDate && startDate ? curDate.getTime() - startDate.getTime() : '';
+}
+
+function msToReadableTime(ms) {
+    let seconds = Math.floor(ms / 1000);
+    let minutes = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    let hours = Math.floor(minutes / 60);
+    minutes = minutes % 60;
+
+    return [hours, minutes, seconds].map(function(el) {
+        el = Math.floor(el).toString();
+        return el.length == 1 ? '0' + el : el;
+    }).join(':');
+}
