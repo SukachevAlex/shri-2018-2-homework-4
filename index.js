fc80d21900f30d738d72e7599879a149d82ab367
SukachevAlex <sukachevalexandr@gmail.com>
2018-10-16 01:48:29 +0300

    Добавил обработчик дефолтных роутов
    Отдается статус 404 и заголовок h1, что страница не найдена.

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(port, () => console.log(`Working on port ${port}`));
