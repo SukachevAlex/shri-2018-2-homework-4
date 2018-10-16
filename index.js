c96d80c9567bca6de6d6c2ac256099276c2dca5c
SukachevAlex <sukachevalexandr@gmail.com>
2018-10-16 14:57:01 +0300

    Добавил обработку роуту /api/events, добавил events.json
    Для данного роута пост запрос рекваирит events.json и с помощью res.json выдает файл.
    Для гет запроса используется потоки. Если роут не содержит type, или приходит что-то отличное от type, то выдается файл с помощью потоков.
    Если в роуте указан корректный type, который содержится в events.json, то фильтруется events.json и выдается нужные данные.
    Если некорректный type, то выдается "incorrect type" со статусом 400.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const dataFile = require('./data/events.json');
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 8000;
 const startingDate = new Date();
@@ -8,12 +10,51 @@ app.all('/status', (req, res) => {
     res.send(msToReadableTime(diff));
 })
 
+app.route('/api/events')
+    .get((req, res) => {
+        const chunks = {
+            events: []
+        };
+        const stream = fs.createReadStream(__dirname + '/data/events.json');
+        res.header("Content-Type", "text/cache-manifest");
+
+        if (req.query.type) {
+            let types = req.query.type.split(':');
+            stream.on('data', function(record) {
+                JSON.parse(record).events.forEach(element => {
+                    if (types.includes(element.type)) {
+                        chunks.events.push(element);
+                    }
+                });
+            });
+            stream.on('error', () => {
+                console.log('error reading stream');
+            })
+            stream.on('end', function() {
+                if (chunks.events.length) {
+                    res.send(JSON.stringify(chunks));
+                } else {
+                    res.status(400).send('incorrect type');
+                }
+            })
+        } else {
+            stream.pipe(res);
+        }
+    })
+    .post((req, res) => {
+        res.json(dataFile);
+    });
+
 app.all('*', (req, res) => {
     res.status(404).send('<h1>Page not found</h1>');
 });
 
-app.listen(port, () => console.log(`Working on port ${port}`));
-
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('error listening port', err);
+    }
+    console.log(`Working on port ${port}`);
+});
 
 function getDiffBetweenDates(startDate, curDate) {
     return curDate && startDate ? curDate.getTime() - startDate.getTime() : '';
