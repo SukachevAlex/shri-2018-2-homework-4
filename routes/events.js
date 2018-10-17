e00847d419a0b0c0ad737b20a488ff0fc4c859b7
SukachevAlex <sukachevalexandr@gmail.com>
2018-10-17 15:34:58 +0300

    Сделал пагинацию событий.
    Используются параметры offset и limit.

--- routes/events.js
+++ routes/events.js
@@ -6,27 +6,31 @@ const dataFile = require('../data/events.json');
 
 router
     .get('/api/events', (req, res) => {
-        const chunks = {
-            events: []
-        };
+        let chunks = [];
         const stream = fs.createReadStream(path.resolve('./data/events.json'));
         res.header("Content-Type", "text/cache-manifest");
 
-        if (req.query.type) {
-            let types = req.query.type.split(':');
+        let types = req.query.type ? req.query.type.split(':') : null;
+        let offset = req.query.offset && req.query.offset > 0 ? +req.query.offset : 0;
+        let limit = req.query.limit && req.query.limit > 0 ? +offset + +req.query.limit : -1;
+
+        if (types || offset || limit) {
             stream.on('data', function(record) {
-                JSON.parse(record).events.forEach(element => {
-                    if (types.includes(element.type)) {
-                        chunks.events.push(element);
-                    }
-                });
+                if (types) {
+                    chunks = JSON.parse(record).events.filter(el =>
+                        types.includes(el.type));
+                } else {
+                    JSON.parse(record).events.forEach(element => {
+                        chunks.push(element);
+                    });
+                }
             });
             stream.on('error', () => {
                 console.log('error reading stream');
             })
             stream.on('end', function() {
-                if (chunks.events.length) {
-                    res.send(JSON.stringify(chunks));
+                if (chunks.length) {
+                    res.send(JSON.stringify(chunks.slice(offset, limit)));
                 } else {
                     res.status(400).send('incorrect type');
                 }
